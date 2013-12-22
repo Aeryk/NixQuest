e7edc4efd89a30e4cd719d5bae48c3d4e69e6d66
Aeryk <aeryk@aeryk.net>
2013-12-22 05:13:38 -0800

    Added Events.py
    Added Events.py. Recorded current issues, added Event object. Prepped
    Event for basic features.
    
    Lotsa unsolved problems. PreAlpha.

---
+++ Events.py
@@ -0,0 +1,21 @@
+"""
+    This class should receive the .txt file that has any given event's read text, then receive the next event to go to and pass that event's call.
+    
+    ****CURRENT ISSUES****
+    1) I have no idea how to receive an answer when the number of choices varies between one and seven. Not a five AM problem.
+    2) I'm not actually sure this'll refer to the correct file on the "with open" line.
+    3) I don't know, at all, how to import this. Do you just import Events.py? Will that work? Do we call it "Events" instead?
+    4) I know that answerList will be a list we'll prep, but I'm not sure how. Or what we'll actually do with it.
+"""
+class Event(object):
+    def __init__(self, eventTextFile, answerList):
+        self.eventTextFile = eventTextFile
+        self.answerList = answerList
+    
+
+    def printBody():#
+        with open("bodyFile", "r") as file:
+            print file.read()
+
+    def chooseAnswer():
+        pass
