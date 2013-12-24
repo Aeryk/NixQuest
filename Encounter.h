d579bad9712059f3817fb017425cecbfe8150218
Aeryk <aeryk@aeryk.net>
2013-12-24 14:32:44 -0800

    Added blank class Encounter
    Added a blank Encounter class that can be copied and used as a template
    for future blank classes.

---
+++ Encounter.h
@@ -0,0 +1,12 @@
+//
+//  Encounter.h
+//  
+//
+//  Created by Aeryk William Blair on 12/24/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Encounter : NSObject
+@end
---
+++ Encounter.m
@@ -0,0 +1,12 @@
+//
+//  Encounter.m
+//  
+//
+//  Created by Aeryk William Blair on 12/24/13.
+//
+//
+
+#import "Encounter.h"
+
+@implementation Encounter
+@end
