e4c0bcad54ea63827970ab24e1ee72a32cf21773
Alexandria <kipitry@icloud.com>
2014-12-25 16:17:49 -0800

    added the Inventory Dict class
    nothing in said class yet

---
+++ NixQuest/NixQuest/Model/Inventory Dictionary.h
@@ -0,0 +1,13 @@
+//
+//  Inventory Dictionary.h
+//  NixQuest
+//
+//  Created by Alexandria Hart on 12/25/14.
+//  Copyright (c) 2014 Aislin Software and Design. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Inventory_Dictionary : NSObject
+
+@end
---
+++ NixQuest/NixQuest/Model/Inventory Dictionary.m
@@ -0,0 +1,13 @@
+//
+//  Inventory Dictionary.m
+//  NixQuest
+//
+//  Created by Alexandria Hart on 12/25/14.
+//  Copyright (c) 2014 Aislin Software and Design. All rights reserved.
+//
+
+#import "Inventory Dictionary.h"
+
+@implementation Inventory_Dictionary
+
+@end
